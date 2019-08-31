a65f2fe6b3cab12a52faf0d018f323c5574ada96
mrnot <33273838+mrnot@users.noreply.github.com>
2019-08-31 11:39:04 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>ThaiCreate.Com PHP/MySQL (Thai UTF8)</title>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+</head>
+<body>
+
+<?php 
+
+echo "ภาษาไทย";
+?>
+</body>
+</html> 
